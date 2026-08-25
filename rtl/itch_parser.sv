f38f768cfbb5cc05d2818f6775706a67f00eac99
Michal Cedro <mcedro@student.agh.edu.pl>
2026-08-25 23:13:02 +0200

    FSM

--- rtl/itch_parser.sv
+++ rtl/itch_parser.sv
@@ -8,23 +8,29 @@ module itch_parser (
     output logic      s_axis_tready,
 
     output logic [7:0] msg_type,
+    output logic [7:0] msg_data,
     output logic       msg_valid
 );
 
     assign s_axis_tready = 1'b1; 
 
-    typedef enum logic {
-        ST_IDLE = 1'b0,
-        ST_READ = 1'b1
+    typedef enum logic [2:0] {
+        ST_IDLE       = 3'd0,
+        ST_MOLD_HDR    = 3'd1,
+        ST_MSG_LENGTH = 3'd2,
+        ST_MSG_TYPE   = 3'd3,
+        ST_PAYLOAD    = 3'd4  
     } state_t;
 
     state_t             state;
     logic [15:0]        byte_cnt;
+    logic [15:0]        msg_length;
 
     always_ff @(posedge clk) begin
         if (!rst_n) begin
             msg_valid <= 0;
             msg_type  <= 8'h00;
+            msg_data  <= 8'h00;
             state     <= ST_IDLE;
             byte_cnt  <= 0;
         end 
@@ -34,20 +40,47 @@ module itch_parser (
             if (s_axis_tvalid) begin
                 case (state)
                     ST_IDLE: begin
-                        state    <= ST_READ;
+                        state    <= ST_MOLD_HDR;
                         byte_cnt <= 1;
                     end
 
-                    ST_READ: begin
+                    ST_MOLD_HDR: begin
                         if (byte_cnt == 19) begin
-                            msg_type  <= s_axis_tdata;
-                            msg_valid <= 1'b1;
-                        end
-
-                        if (s_axis_tlast) begin
-                            state <= ST_IDLE;
+                            state    <= ST_MSG_LENGTH;
+                            byte_cnt <= 0;
                         end else begin
                             byte_cnt <= byte_cnt +1;
+                        end
+                    end
+                    
+                    ST_MSG_LENGTH: begin
+                        msg_length <= {msg_length[7:0], s_axis_tdata};
+
+                        if (byte_cnt == 1) begin
+                            state    <= ST_MSG_TYPE;
+                            byte_cnt <= 0;
+                        end else begin
+                            byte_cnt <= byte_cnt + 1;
+                        end
+                    end
+
+                    ST_MSG_TYPE: begin
+                        msg_type  <= s_axis_tdata;
+                        msg_data  <= s_axis_tdata;
+                        msg_valid <= 1'b1;
+
+                        state <=ST_PAYLOAD;
+                        byte_cnt <= 1;
+                    end
+
+                    ST_PAYLOAD: begin
+                        msg_valid <= 1'b1;
+                        msg_data  <= s_axis_tdata;
+
+                        if (byte_cnt == msg_length - 1 || s_axis_tlast) begin
+                            state <= ST_IDLE;
+                        end else begin
+                            byte_cnt <= byte_cnt + 1;
                         end
                     end
                 endcase
--- sim/itch_parser_tb.sv
+++ sim/itch_parser_tb.sv
@@ -9,6 +9,7 @@ module itch_parser_tb;
     logic       s_axis_tready;
 
     logic [7:0] msg_type;
+    logic [7:0] msg_data;
     logic       msg_valid;
 
     itch_parser dut (
@@ -19,6 +20,7 @@ module itch_parser_tb;
         .s_axis_tready(s_axis_tready),
         .s_axis_tvalid(s_axis_tvalid),
         .msg_type(msg_type),
+        .msg_data(msg_data),
         .msg_valid(msg_valid)
     );
 
@@ -32,6 +34,35 @@ module itch_parser_tb;
         s_axis_tlast = 0;
 
         #20 rst_n = 1;
+
+        #20;
+
+        $display("Wysylam naglowek MoldUDP64...");
+        for (int i = 0; i < 20; i++) begin
+            @(posedge clk);          
+            s_axis_tvalid <= 1'b1;   
+            s_axis_tdata  <= 8'h00;  
+            s_axis_tlast  <= 1'b0;
+        end
+
+        $display("Wysylam Dlugosc wiadomosci...");
+        @(posedge clk); s_axis_tdata <= 8'h00;
+        @(posedge clk); s_axis_tdata <= 8'h04; 
+
+        $display("Wysylam typ wiadomosci ITCH...");
+        @(posedge clk); s_axis_tdata <= 8'h41; 
+
+        $display("Wysylam Payload...");
+        @(posedge clk); s_axis_tdata <= 8'hBB;
+        @(posedge clk); s_axis_tdata <= 8'hCC;
+        @(posedge clk); 
+        s_axis_tdata <= 8'hDD;
+        s_axis_tlast <= 1'b1; 
+
+        @(posedge clk);
+        s_axis_tvalid <= 1'b0;
+        s_axis_tlast  <= 1'b0;
+        s_axis_tdata  <= 8'h00;
 
         #100 $finish;
     end
